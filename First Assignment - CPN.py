f8099730e7cd74365e333f4c96aa2eef2125a5ec
phoenix507r <73867880+phoenix507r@users.noreply.github.com>
2021-02-28 22:22:45 -0700

    Add files via upload

---
+++ First Assignment - CPN.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the notebook that will be used for the capstone project
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
