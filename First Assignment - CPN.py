e6ad570cb59bf17ec50320675086d9047d19d7b5
phoenix507r <73867880+phoenix507r@users.noreply.github.com>
2021-02-28 22:23:30 -0700

    Update First Assignment - CPN.py

--- First Assignment - CPN.py
+++ First Assignment - CPN.py
@@ -3,17 +3,9 @@
 
 # This is the notebook that will be used for the capstone project
 
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 
 print('Hello Capstone Project Course!')
-
-
-# In[ ]:
-
-
 
 
